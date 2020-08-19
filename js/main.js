e1d06209e36939f1b4cfe101d4ae66c5434c0fc8
Webdevmaksim <maksim.saraev.121.5@gmail.com>
2020-08-19 19:32:12 +0300

    feat: arrow

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+    //Кнопка прокрутки наверх
+    $(window).scroll(function () {
+        // Если отступ сверху больше 50px то показываем кнопку "Наверх"
+        if ($(this).scrollTop() > 50) {
+            $('.button-up').fadeIn();
+        } else {
+            $('.button-up').fadeOut();
+        }
+    });
+    
+    /** При нажатии на кнопку мы перемещаемся к началу страницы */
+    $('.button-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
     //wowJS
     new WOW().init();
 });
